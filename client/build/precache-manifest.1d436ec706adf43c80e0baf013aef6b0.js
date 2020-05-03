e19ef7b5151accb1bb89c9f153280c6032e44e2b
AntonTrofymchuk <antontrofymchuk@gmail.com>
2020-05-03 14:22:27 +0300

    added build

---
+++ client/build/precache-manifest.1d436ec706adf43c80e0baf013aef6b0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c9e3bd9552cf821210634c250ec15d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "770efa2c9b83f39d5a16",
+    "url": "/static/css/2.35e45882.chunk.css"
+  },
+  {
+    "revision": "2d89187e068feb16d1ca",
+    "url": "/static/css/main.bbc11d31.chunk.css"
+  },
+  {
+    "revision": "770efa2c9b83f39d5a16",
+    "url": "/static/js/2.6b4849ed.chunk.js"
+  },
+  {
+    "revision": "eb7fbd7393375b18039e581d34869e2d",
+    "url": "/static/js/2.6b4849ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d89187e068feb16d1ca",
+    "url": "/static/js/main.3b90e000.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  },
+  {
+    "revision": "15f33718ef523ed8a5c11114abd7b219",
+    "url": "/static/media/1.15f33718.jpg"
+  },
+  {
+    "revision": "4b150143efd99ed9cd702b8730809e37",
+    "url": "/static/media/2.4b150143.jpg"
+  },
+  {
+    "revision": "6f3cce1caa4fc0c6aeaf68e54696297a",
+    "url": "/static/media/3.6f3cce1c.jpg"
+  },
+  {
+    "revision": "f7f17eba092266672fd566276c495147",
+    "url": "/static/media/author.f7f17eba.png"
+  }
+]);
